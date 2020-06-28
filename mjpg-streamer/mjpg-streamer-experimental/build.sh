b46428e9488d4067f812a1159a28a3993f28b3fe
weesing <posttomichael@gmail.com>
2020-06-28 15:43:20 +0800

    Slash missing.

--- mjpg-streamer/mjpg-streamer-experimental/build.sh
+++ mjpg-streamer/mjpg-streamer-experimental/build.sh
@@ -5,7 +5,7 @@ docker save weesing/mjpg-streamer:latest | gzip > build/weesing-mjpg-streamer.ta
 echo "DONE"
 
 echo "Copying over to Raspberry Pi..."
-scp build/weesing-mjpg-streamer.tar.gz raspi:~/octoprint/mjpg-streamer/mjpg-streamer-experimental/build
+scp build/weesing-mjpg-streamer.tar.gz raspi:~/octoprint/mjpg-streamer/mjpg-streamer-experimental/build/
 echo "DONE"
 
 echo "You can now SSH to your Raspberry Pi and run the exec.sh script."
