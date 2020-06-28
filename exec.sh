e018d4a203ff7ef36835c3f9f2a0d6a49b8864c8
weesing <posttomichael@gmail.com>
2020-06-28 19:38:10 +0800

    Final exec.sh

--- exec.sh
+++ exec.sh
@@ -33,16 +33,10 @@ pushd mjpg-streamer/mjpg-streamer-experimental
 export LD_LIBRARY_PATH=.
 make
 
-# sudo cp mjpg_streamer /usr/bin
-# sudo chmod u+x /usr/bin/mjpg_streamer
-
-# sudo cp mjpg_streamer.service /etc/systemd/system/mjpg_streamer.service
-# sudo systemctl stop mjpg_streamer
-# sudo systemctl start mjpg_streamer
-# sudo systemctl enable mjpg_streamer
-# sudo systemctl daemon-reload
-# sudo adduser --system --ingroup video mjpg_streamer
-
-nohup ./mjpg_streamer -i "./input_raspicam.so -fps 5" -o "./output_http.so"
+sudo cp mjpg_streamer.service /etc/systemd/system/mjpg_streamer.service
+sudo systemctl stop mjpg_streamer
+sudo systemctl start mjpg_streamer
+sudo systemctl enable mjpg_streamer
+sudo systemctl daemon-reload
 
 popd
