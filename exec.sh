9f35c1d8deb66b340c02949d520a58684867e560
Yap Wee Sing <posttomichael@gmail.com>
2020-06-28 18:17:25 +0800

    new mjpg_streamer.service

--- exec.sh
+++ exec.sh
@@ -30,8 +30,17 @@ sudo apt install subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cm
 git clone https://github.com/jacksonliam/mjpg-streamer.git
 cd mjpg-streamer && rm -rf .git
 
-cd mjpg-streamer/mjpg-streamer-experimental
+pushd mjpg-streamer/mjpg-streamer-experimental
 export LD_LIBRARY_PATH=.
 make
+
+sudo cp mjpg_streamer /usr/bin
+sudo chmod u+x /usr/bin/mjpg_streamer
+
+sudo cp mjpg_streamer.service /etc/systemd/system/mjpg_streamer.service
+sudo systemctl stop mjpg_streamer
+sudo systemctl start mjpg_streamer
+sudo systemctl enable mjpg_streamer
+sudo systemctl daemon-reload
 
 sudo adduser --system --ingroup video mjpg_streamer
