4b5c04e96bf47cf81c6358d9211158a1737d2ed3
weesing <posttomichael@gmail.com>
2020-06-28 15:39:42 +0800

    Adding step for scp in build.sh

--- mjpg-streamer/mjpg-streamer-experimental/build.sh
+++ mjpg-streamer/mjpg-streamer-experimental/build.sh
@@ -1,2 +1,11 @@
 docker build -t weesing/mjpg-streamer .
+
+echo "Exporting image to build/weesing-mjpg-streamer.tar.gz..."
 docker save weesing/mjpg-streamer:latest | gzip > build/weesing-mjpg-streamer.tar.gz
+echo "DONE"
+
+echo "Copying over to Raspberry Pi..."
+scp build/weesing-mjpg-streamer.tar.gz raspi:~/octoprint/mjpg-streamer/mjpg-streamer-experimental/build
+echo "DONE"
+
+echo "You can now SSH to your Raspberry Pi and run the exec.sh script."
