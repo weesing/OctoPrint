49b9e9ee8bcb24ef7aa766aec301e8480522e7eb
weesing <posttomichael@gmail.com>
2020-06-28 16:36:49 +0800

    Make building part of build.sh

--- mjpg-streamer/mjpg-streamer-experimental/exec.sh
+++ mjpg-streamer/mjpg-streamer-experimental/exec.sh
@@ -4,9 +4,13 @@ docker rm mjpg-streamer
 docker rmi weesing/mjpg-streamer:latest
 echo "DONE"
 
-echo "Loading image build/weesing-mjpg-streamer.tar.gz..."
-docker load < build/weesing-mjpg-streamer.tar.gz
+echo "Building image..."
+docker build -t weesing/mjpg-streamer .
 echo "DONE"
+
+# echo "Loading image build/weesing-mjpg-streamer.tar.gz..."
+# docker load < build/weesing-mjpg-streamer.tar.gz
+# echo "DONE"
 
 echo "Starting container..."
 docker run --name mjpg-streamer -d -p 8081:8081 weesing/mjpg-streamer
