4306dea05922205965c8af7f8916a9ecd3ba500c
weesing <posttomichael@gmail.com>
2020-06-28 15:35:41 +0800

    Making exec.sh script more user-friendly.

--- mjpg-streamer/mjpg-streamer-experimental/exec.sh
+++ mjpg-streamer/mjpg-streamer-experimental/exec.sh
@@ -1,5 +1,15 @@
+echo "Removing current containers and images..."
 docker stop mjpg-streamer
 docker rm mjpg-streamer
 docker rmi weesing/mjpg-streamer:latest
+echo "DONE"
+
+echo "Loading image build/weesing-mjpg-streamer.tar.gz..."
 docker load < build/weesing-mjpg-streamer.tar.gz
+echo "DONE"
+
+echo "Starting container..."
 docker run --name mjpg-streamer -d -p 8081:8081 weesing/mjpg-streamer
+echo "DONE"
+
+docker logs -f mjpg-streamer
