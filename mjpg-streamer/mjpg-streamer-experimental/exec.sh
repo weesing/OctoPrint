503fa2a33479cf12e9fc9babee8e2338041ed776
Yap Wee Sing <posttomichael@gmail.com>
2020-06-28 17:03:10 +0800

    Changed image name to run.

--- mjpg-streamer/mjpg-streamer-experimental/exec.sh
+++ mjpg-streamer/mjpg-streamer-experimental/exec.sh
@@ -13,7 +13,7 @@ echo "DONE"
 # echo "DONE"
 
 echo "Starting container..."
-docker run --name mjpg-streamer  -d -t -i -p 8081:8081/tcp --device=/dev/video0 image:tag
+docker run --name mjpg-streamer  -d -t -i -p 8081:8081/tcp --device=/dev/video0 weesing/mjpg-streamer:latest
 echo "DONE"
 
 docker logs -f mjpg-streamer
