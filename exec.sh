91a7abf1b1c90d5b0ed017c0e61a1dfb51b452ec
Yap Wee Sing <posttomichael@gmail.com>
2020-06-28 18:41:57 +0800

    Commetned off mjpg streamer stuff

--- exec.sh
+++ exec.sh
@@ -26,21 +26,21 @@ sudo cp config/haproxy.cfg /etc/haproxy/
 sudo systemctl enable haproxy
 sudo systemctl start haproxy
 
-sudo apt install subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake
-git clone https://github.com/jacksonliam/mjpg-streamer.git
-cd mjpg-streamer && rm -rf .git
+# sudo apt install subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake
+# git clone https://github.com/jacksonliam/mjpg-streamer.git
+# cd mjpg-streamer && rm -rf .git
 
-pushd mjpg-streamer/mjpg-streamer-experimental
-export LD_LIBRARY_PATH=.
-make
+# pushd mjpg-streamer/mjpg-streamer-experimental
+# export LD_LIBRARY_PATH=.
+# make
 
-sudo cp mjpg_streamer /usr/bin
-sudo chmod u+x /usr/bin/mjpg_streamer
+# sudo cp mjpg_streamer /usr/bin
+# sudo chmod u+x /usr/bin/mjpg_streamer
 
-sudo cp mjpg_streamer.service /etc/systemd/system/mjpg_streamer.service
-sudo systemctl stop mjpg_streamer
-sudo systemctl start mjpg_streamer
-sudo systemctl enable mjpg_streamer
-sudo systemctl daemon-reload
+# sudo cp mjpg_streamer.service /etc/systemd/system/mjpg_streamer.service
+# sudo systemctl stop mjpg_streamer
+# sudo systemctl start mjpg_streamer
+# sudo systemctl enable mjpg_streamer
+# sudo systemctl daemon-reload
 
-sudo adduser --system --ingroup video mjpg_streamer
+# sudo adduser --system --ingroup video mjpg_streamer
