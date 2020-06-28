499b7b05514ba651b30a978d40faf5b8d47424b9
weesing <posttomichael@gmail.com>
2020-06-28 15:13:44 +0800

    Change the source of the image build in exec.sh

--- mjpg-streamer/mjpg-streamer-experimental/exec.sh
+++ mjpg-streamer/mjpg-streamer-experimental/exec.sh
@@ -1,5 +1,5 @@
 docker stop mjpg-streamer
 docker rm mjpg-streamer
 docker rmi weesing/mjpg-streamer:latest
-docker load < weesing-mjpg-streamer.tar.gz
+docker load < build/weesing-mjpg-streamer.tar.gz
 docker run --name mjpg-streamer -d -p 8081:8081 weesing/mjpg-streamer
