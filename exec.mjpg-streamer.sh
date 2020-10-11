3b2d763a0c0e9ef52ba2010994333dc00b4a0c78
yapweesing <posttomichael@gmail.com>
2020-10-11 12:57:32 +0800

    Deleted unecessary shell scripts. renamed exec.sh to install.sh

--- exec.mjpg-streamer.sh
+++
@@ -1,9 +0,0 @@
-pushd mjpg-streamer/mjpg-streamer-experimental
-
-sudo cp mjpg_streamer.service /etc/systemd/system/mjpg_streamer.service
-sudo systemctl stop mjpg_streamer
-sudo systemctl start mjpg_streamer
-sudo systemctl enable mjpg_streamer
-sudo systemctl daemon-reload
-
-popd
